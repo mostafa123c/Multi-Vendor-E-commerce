097ac89e035f7bcf1c23b3228d5e4ad7e5fe496f
mostafa123c <mostafaemad112744@gmail.com>
2023-02-23 17:20:29 +0200

    pagination

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -20,7 +20,17 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();        //return collection
+        $request = request();
+        $query = Category::query();
+
+        if($name = $request->query('name')) {
+            $query->where('name' , 'LIKE' , "%{$name}%");
+        }
+        if($status = $request->query('status')) {
+            $query->where('status' , '=' , $status);
+        }
+
+        $categories = $query->paginate(2);        //return collection object
         return view('dashboard.categories.index' , compact('categories'));
     }
 
--- resources/views/Dashboard/categories/index.blade.php
+++ resources/views/Dashboard/categories/index.blade.php
@@ -16,6 +16,17 @@
     <x-alert type="success" />
     <x-alert type="info" />
 
+
+    <form action="{{ URL::current() }}" method="get" class="d-flex justify-content-between mb-4">
+        <x-form.input name="name" placeholder="Name" class="mx-2" :value="request('name')" />
+        <select name="status" class="form-control mx-2">
+            <option value="">All</option>
+            <option value="active" @selected(request('status') == 'active')>Active</option>
+            <option value="archived" @selected(request('status') == 'archived')>Archived</option>
+        </select>
+        <button class="btn btn-dark mx-2">Filter</button>
+    </form>
+
     <table class="table">
         <thead>
         <tr>
@@ -23,6 +34,7 @@
             <th>ID</th>
             <th>Name</th>
             <th>Parent</th>
+            <th>Status</th>
             <th>Created At</th>
             <th colspan="2"></th>
         </tr>
@@ -34,6 +46,7 @@
                 <td>{{$category->id}}</td>
                 <td>{{$category->name}}</td>
                 <td>{{$category->parent_id}}</td>
+                <td>{{$category->status}}</td>
                 <td>{{$category->created_at}}</td>
                 <td>
                     <a href="{{ route('dashboard.categories.edit' , $category->id) }}" class="btn btn-sm btn-outline-success">Edit</a>
@@ -56,5 +69,7 @@
         </tbody>
 
     </table>
+
+    {{$categories->withquerystring()->links()}}
 @endsection
 
