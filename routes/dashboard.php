e1f947aef2eb75c89e3d9d7c0c321da7e439bf6f
mostafa123c <mostafaemad112744@gmail.com>
2023-03-06 15:55:48 +0200

    ProfileRoutes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\Dashboardcontroller;
 use App\Http\Controllers\Dashboard\ProductsController;
+use App\Http\Controllers\Dashboard\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,6 +14,9 @@ Route::group([
     //'namespace' => 'App\Http\Controllers\Dashboard',
 
 ], function (){
+
+    Route::get('profile' , [ProfileController::class , 'edit'])->name('profile.edit');
+    Route::patch('profile' , [ProfileController::class , 'update'])->name('profile.update');
 
     Route::get('/',[Dashboardcontroller::class , 'index'])->name('dashboard');
 
