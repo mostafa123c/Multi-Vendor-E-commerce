4f6d3b9d332883d784eed916e2dc4136130a9dac
mostafa123c <mostafaemad112744@gmail.com>
2023-03-06 15:53:22 +0200

    ProductsUpdates

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use App\Models\Scopes\StoreScope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'image', 'category_id', 'store_id',
+        'price', 'compare_price', 'status',
+    ];
 
     protected static function booted()
     {
@@ -23,5 +30,43 @@ class Product extends Model
     public function store()
     {
         return $this->belongsTo(Store::class, 'store_id', 'id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,     // Related Model
+            'product_tag',  // Pivot table name
+            'product_id',   // FK in pivot table for the current model
+            'tag_id',       // FK in pivot table for the related model
+            'id',           // PK current model
+            'id'            // PK related model
+        );
+    }
+
+    public function scopeActive(Builder $builder)
+    {
+        $builder->where('status', '=', 'active');
+    }
+
+    // Accessors
+    // $product->image_url
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image) {
+            return 'https://www.incathlab.com/images/products/default_product.png';
+        }
+        if (Str::startsWith($this->image, ['http://', 'https://'])) {
+            return $this->image;
+        }
+        return asset('storage/' . $this->image);
+    }
+
+    public function getSalePercentAttribute()
+    {
+        if (!$this->compare_price) {
+            return 0;
+        }
+        return round(100 - (100 * $this->price / $this->compare_price), 1);
     }
 }
