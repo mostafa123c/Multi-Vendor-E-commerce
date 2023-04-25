af59a8627b1b26eec578a70c7cc250ff98219f7e
mostafa123c <mostafaemad112744@gmail.com>
2023-04-25 15:00:47 +0200

    Currency

--- app/Helpers/Currency.php
+++ app/Helpers/Currency.php
@@ -15,13 +15,18 @@ class Currency
 
     public static function format($amount, $currency = null)
     {
+        $baseCurrency = config('app.currency', 'USD');
 
         $formatter = new NumberFormatter(config('app.locale'), NumberFormatter::CURRENCY);
 
         if ($currency === null) {
-            $currency = config('app.currency' , 'USD');
+            $currency = Session::get('currency_code', $baseCurrency);
         }
 
+        if ($currency != $baseCurrency) {
+            $rate = Cache::get('currency_rate_' . $currency, 1);
+            $amount = $amount * $rate;
+        }
 
         return $formatter->formatCurrency($amount, $currency);
     }
