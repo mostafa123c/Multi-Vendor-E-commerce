9b66a155b66b080cccfbf96d902696682dd1a2a4
mostafa123c <mostafaemad112744@gmail.com>
2023-05-08 12:36:54 +0300

    Admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use App\Concerns\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends User
 {
-    use HasFactory,Notifiable;
+    use HasFactory,Notifiable,HasApiTokens ,HasRoles;
 
     protected $fillable = ['name', 'email', 'password', 'phone_number', 'super_admin', 'status'];
 }
